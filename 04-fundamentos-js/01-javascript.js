48220596af10f2aabc6e1d79fc7d54b747a33d0c
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-17 21:01:54 -0500

    avance

--- 04-fundamentos-js/01-javascript.js
+++ 04-fundamentos-js/01-javascript.js
@@ -19,6 +19,34 @@ let edad = 30; //number
 console.log(typeof nombre);
 console.log(typeof edad);
 
-let estaLloviendo = false;
+let estaLloviendo = false; //boolean
 console.log(estaLloviendo);
 console.log(typeof estaLloviendo);
+
+//tipado dínamico, podemos cambiar el tipo de dato de una variable
+let numero = 20;
+// el console.log() puede recibir varios parámetros
+console.log("Como Número", numero);
+
+numero = "veinteeeee";
+console.log("Como String", numero);
+
+// undefined, aún no esta definido
+let regalo;
+console.log(regalo);
+
+//null, NO ES primitivo
+let nulo = null;
+console.log(nulo);
+console.log(typeof nulo);
+
+let cantidad = "100";
+console.log(typeof cantidad);
+
+console.log(cantidad * 20);
+
+// Aunque podemos utilizar + para concatenar podemos tener problemas al concatenar con números
+console.log("Juan " + "Perez")
+console.log(cantidad + 75);
+
+//en unos momentos lo solucionamos
