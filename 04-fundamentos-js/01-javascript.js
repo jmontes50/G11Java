d48eed292b66590798ec602e7c5ac2681244171c
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-17 20:48:03 -0500

    avance

---
+++ 04-fundamentos-js/01-javascript.js
@@ -0,0 +1,24 @@
+// soy un comentario :D de línea
+/**
+ * y aquí podemos
+ * escribir
+ * varias líneas,
+ * ideas para documentación
+ */
+console.log("Hola mundo!!!");
+
+// a diferencia de otros lenguajes, JS tiene un tipado dinámico
+// let es la forma que declaramos variables
+let nombre = "Juan"; //string
+console.log(nombre);
+nombre = "Juancho";
+console.log(nombre);
+
+let edad = 30; //number
+
+console.log(typeof nombre);
+console.log(typeof edad);
+
+let estaLloviendo = false;
+console.log(estaLloviendo);
+console.log(typeof estaLloviendo);
