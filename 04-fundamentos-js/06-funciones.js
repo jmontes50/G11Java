e38d05fc0c3a014d1828549666a817c565e9d7f3
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-20 20:09:04 -0500

    avance

--- 04-fundamentos-js/06-funciones.js
+++ 04-fundamentos-js/06-funciones.js
@@ -22,3 +22,22 @@ function operacion (a, b, tarea) {
 console.log(operacion(100, 200, sumar));
 
 console.log(operacion(100, 200, resta));
+
+//funciones flecha
+//(parametros) => {codigo a ejecutar}
+// let multiplicacion = (a, b) => {
+//   return a * b;
+// }
+
+//(parametros) => expresión a retornar
+//cuando el codigo es corto
+let multiplicacion = (a, b) => a * b;
+
+console.log(multiplicacion(5, 70));
+
+//cuando tenemos 01 solo parámetro, podemos quitar los parentesís
+// let saludar = (nombre) => `Hola ${nombre}`;
+let saludar = nombre => `Hola ${nombre}`;
+
+console.log("Saludoooo:", saludar("Araceli"));
+
