3ceda9e5ae00ef0f4ff6187e1f9273b2cea3b343
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-20 19:29:02 -0500

    avance

---
+++ 04-fundamentos-js/05-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * No necesito definir el tipo, ni la cantidad, la asignación es =
+ */
+
+let numeros = [10, 20, 30, 40];
+
+console.log(numeros);
+
+console.log(numeros[1]);
+
+numeros[3] = 100;
+
+console.log(numeros);
+
+let cosas = ["Textooo", 10000, true, undefined, "HI", [7,8,9]];
+
+console.log(cosas);
