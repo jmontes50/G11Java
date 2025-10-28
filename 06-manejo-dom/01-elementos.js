3ee57469a1b8ec68dea5048f5cd5ea1d08206002
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-27 21:03:15 -0500

    avance

---
+++ 06-manejo-dom/01-elementos.js
@@ -0,0 +1,8 @@
+//window representa la ventana en si del navegador
+console.log(window.location.href);
+
+console.log(document);
+
+const seccion = document.getElementById("principal");
+
+console.log(seccion);
