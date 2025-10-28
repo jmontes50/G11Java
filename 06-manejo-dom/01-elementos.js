389a5c00c3ee5550659a4a4ca831792a97b9e774
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-27 21:42:54 -0500

    avance

--- 06-manejo-dom/01-elementos.js
+++ 06-manejo-dom/01-elementos.js
@@ -14,5 +14,9 @@ console.log(Titulo1);
 //innerHTML es una propiedad que me permite leer y/o modificar el html interno de un elemento
 Titulo1.innerHTML = "Título modificado desde JS";
 
-//con style podemos acceder a diferentes propiedades de CSS del elemento desde JS
+//con style podemos acceder a diferentes propiedades de CSS del elemento desde JS --> style.nombrePropiedad
 Titulo1.style.color = "red";
+Titulo1.style.padding = "20px";
+//background-color ---kebab-case
+//backgroundColor ---camelCase
+Titulo1.style.backgroundColor = "skyblue";
