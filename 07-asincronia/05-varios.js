3bcdc526ff9d870f2db70d1ecda03ddda93a45e6
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-29 23:01:24 -0500

    avance

---
+++ 07-asincronia/05-varios.js
@@ -0,0 +1,18 @@
+const link = document.querySelector("#link");
+
+link.addEventListener("click", (event) => {
+  event.preventDefault(); //previene la accion por defecto de algo
+  console.log(event);
+  console.log("Click del hipervínculo");
+})
+
+const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //targer objetivo y nos indica de donde surgio el evento
+  //el correo es por el name del input
+  console.log(event.target.correo.value);
+  console.log(event.target.dataset)
+   console.log("Intento de envio del formulario");
+})
