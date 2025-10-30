8981a562ddd56bcc695c361466d9ade56496136e
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-29 19:48:57 -0500

    avance

--- 06-manejo-dom/02-obj-element.js
+++ 06-manejo-dom/02-obj-element.js
@@ -8,6 +8,8 @@ console.log(typeof Seccion1.innerHTML);
 
 const frutas = ["Naranja", "Kiwi", "Chirimoya"];
 
+//Opt1: Inyectando HTML
+
 //hasta que ese string que tiene ul id frutas... no sea procesado por el navegador es un simple texto
 Seccion1.innerHTML += `<ul id="frutas"></ul>`;
 
@@ -20,3 +22,17 @@ frutas.forEach((fru) => {
   // Seccion1.innerHTML += `<ul><li>${fru}</li></ul>`
   ulFrutas.innerHTML += `<li>${fru}</li>`
 })
+
+//Opt2: Creando Elementos
+const Seccion2 = document.querySelector("#seccion2");
+
+//createElement, crea un obj según el nombre de la etiqueta que le indiquemos
+const objImg = document.createElement("img");
+
+console.log(objImg);
+
+objImg.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMKEOcBxdChLlH9V9_yIVUmu61093Vwg2HLg&s");
+
+//agrega un elemento como hijo de otro después del resto de elementos
+//es como un .push pero para elementos html
+Seccion2.appendChild(objImg);
