45f610e64c4841e0753ae8cf2133cfc08153c721
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-31 20:22:55 -0500

    avance

--- 08-node/index.js
+++ 08-node/index.js
@@ -8,7 +8,8 @@ const getData = async () => {
   try {
     //A diferencia de fetch ese objeto no combina rpta con el codigo. Lo da directo
     const response = await axios.get("https://68afa145b91dfcdd62bcb6b1.mockapi.io/prod_vanilla");
-    console.log(response);
+    console.log(response.data);
+    console.log("fin de petición")
   } catch (error) {
     console.log(error)
   }
