07483f128f9aad656c026916b021bb59db53552f
Jorge Montesinos <osmarmontes50@gmail.com>
2025-10-31 22:51:42 -0500

    avance

--- 09-react/02-hooks/src/App.jsx
+++ 09-react/02-hooks/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   // const [getter, setter] = useState(valorInicial)
+  //parte del trabajo del setState es indicar a React que debe actualizar la interfaz]
+  //cada vez que haya un cambio de estado (y también de props) el componente se volverá a renderizar
   const [contador, setContador] = useState(0);
 
   const aumentar = () => {
     console.log("Hola desde aumentar!");
-    contador++;
+    // contador++; //no funciona al menos no para actualizar la interfaz
+    setContador(contador + 1);
   }
 
   return (
