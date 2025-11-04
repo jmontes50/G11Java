e0d6702f405daac5355bee7447af60f432d39b2a
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-03 21:03:49 -0500

    avance

--- 09-react/03-effect/src/App.jsx
+++ 09-react/03-effect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const App = () => {
   // [getter, setter]
@@ -12,32 +12,36 @@ const App = () => {
     //target es de donde si disparo el evento
     // console.log(evento.target.value)
     setTexto(evento.target.value);
-  }
+  };
 
   //lo que hace useEffect es escuchar los cambios de todos los estados y cuando escucha alguno se ejecuta
   useEffect(() => {
     console.log(contador);
-    console.log(texto)
+    console.log(texto);
   }, [texto]);
 
   useEffect(() => {
     console.log("2. Dependencias Vacias en el useEffect");
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((rpta) => rpta.json())
-    .then((data) => {
-      console.log(data);
-      setPosts(data);
-    })
-    .catch((err) => console.log(err))
+      .then((rpta) => rpta.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => console.log(err));
     // si a las dependencias del useEffect, las dejamos como un array vacio, esta solo se ejecutará una vez
-  },[])
+  }, []);
 
   return (
     <div>
       {console.log("3.estoy dentro del return")}
       {console.log("3.1 estoy dentro del return también", posts)}
       <h2>Contador Valor: {contador}</h2>
-      <button onClick={ () => { setContador(contador+1) } }>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
         Aumentar!!!
       </button>
       <hr />
@@ -49,8 +53,22 @@ const App = () => {
         // onChange hace referencia a cuando un input tiene un cambio de valor
         onChange={manejarInput}
       />
+      <hr />
+      <h2>Posts</h2>
+      <ul>
+        {/* renderizado de listas, se refiere a como a partir de un Array podemos transformarlo en JSX */}
+        {/* array -> varios li */}
+        {/* {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>
+        })} */}
+        {/* React usa () cuando va a retornar directamente JSX, aquí ya no es necesario el return */}
+        {posts.map((post) => (
+          //aplican las reglas generales de JSX
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
