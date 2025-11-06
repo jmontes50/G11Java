b63492793c50ff8280f93a2a13830b5ec6ec6681
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-05 19:53:53 -0500

    avance

--- Proyectos/05-admin/src/App.jsx
+++ Proyectos/05-admin/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeView from "./views/HomeView";
 import CreateProductView from "./views/CreateProductView";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     // BR, Una envoltura que nos conecta a la history API de js
     <BrowserRouter>
+      {/* afuera de Routes podemos colocar JSX */}
+      <Navbar />
       <Routes>
         {/* Route representa cada vista con su componente */}
         <Route path="/" element={<HomeView />} />
