d351d05c4b85b387291b9ee4f6364f7ed10b2155
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-05 22:47:04 -0500

    avance

--- Proyectos/05-admin/src/views/UpdateProductView.jsx
+++ Proyectos/05-admin/src/views/UpdateProductView.jsx
@@ -2,10 +2,36 @@
 //https://dominio.com/editar/dsdhisuad
 ///editar/:id ---> { id:dsdhisuad }
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { readProductById } from "../services/productosService";
 
 const UpdateProductView = () => {
+  const [product, setProduct] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    stock: 0,
+    sku: "",
+    activo: true,
+    imagen: "",
+    categoriaId: 1,
+  });
+
   const { id } = useParams();
-  console.log(id);
+  // console.log(id);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const productObtained = await readProductById(id);
+        // console.log(productObtained);
+        setProduct(productObtained);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProduct();
+  },[]);
 
   return (
     <div>UpdateProductView</div>
