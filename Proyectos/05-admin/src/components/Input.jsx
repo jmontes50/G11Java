b1a7211dec42e14b2e4da1eb621c48bdf0720c5b
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-05 20:20:38 -0500

    avance

---
+++ Proyectos/05-admin/src/components/Input.jsx
@@ -0,0 +1,12 @@
+const Input = ({ name, label }) => {
+  return (
+    <div className="mb-3 p-2">
+      <label className="block p-1">
+        {label}
+      </label>
+      <input />
+    </div>
+  )
+}
+
+export default Input
--- Proyectos/05-admin/src/views/CreateProductView.jsx
+++ Proyectos/05-admin/src/views/CreateProductView.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import Input from "../components/Input";
 
 const CreateProductView = () => {
-  return (
-    <div>CreateProductView</div>
-  )
-}
+  const [product, setProduct] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    stock: 0,
+    sku: "",
+    activo: true,
+    imagen: "",
+    categoriaId: 0,
+  });
 
-export default CreateProductView
+  const inputsInfo = [
+    {
+      name: "nombre",
+      label: "Nombre",
+      type: "text",
+    },
+    { name: "descripcion", label: "Descripción del producto", type: "text" },
+    { name: "precio", label: "Precio", type: "number" },
+  ];
+
+  const handleInput = (ev) => {
+    const value = ev.target.value;
+    setProduct({ ...product, nombre: value });
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto p-4">
+      <h1 className="text-2xl font-semibold pb-3">Crear Producto</h1>
+      <form>
+        {inputsInfo.map((item, index) => (
+          <Input
+            key={index}
+            name={item.name}
+            label={item.label}
+          />
+        ))}
+        {/* <input
+          type="text"
+          name="nombre"
+          placeholder="Nombre"
+          value={product.nombre}
+          onChange={handleInput}
+        /> */}
+      </form>
+    </div>
+  );
+};
+
+export default CreateProductView;
